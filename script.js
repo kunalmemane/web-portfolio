eb1233daa9da2a2db9ddb1dbd8add58054c659f1
Kunal <kunalmemane9150@gmail.com>
2024-07-10 21:51:37 +0530

    1.About section ui with company logo -update 2.Navigation close on outside click -feature

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function toggleMenu(){
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
 
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+    event.stopPropagation();
 }
 
-document.addEventListener('contextmenu', event => event.preventDefault());
+// Close toggle on menu item click
+const menuToggler = document.querySelector('.hamburger-icon');
+const body = document.body;
+const navItems = document.querySelectorAll(".nav-item");
+
+Array.from(navItems).map(el => {
+    el.addEventListener('click', function () {
+        body.classList.remove('open');
+    });
+});
+
+// Close toggle on outside click if menu is open
+document.addEventListener('click', function (event) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        icon.classList.remove('open');
+    }
+});
